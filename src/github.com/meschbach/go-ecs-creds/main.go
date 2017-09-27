ace6e9244082eec8c3f759c1e6a16c95ab669b2c
Mark Eschbach <mark@rhumbix.com>
2017-09-27 16:53:21 -0700

    Correctly report errors

--- src/github.com/meschbach/go-ecs-creds/main.go
+++ src/github.com/meschbach/go-ecs-creds/main.go
@@ -21,21 +21,22 @@ func main() {
 	http.HandleFunc( "/", func( w http.ResponseWriter, r *http.Request ) {
 		result, err := svc.GetCallerIdentity(input)
 		if err != nil {
-				if aerr, ok := err.(awserr.Error); ok {
-						switch aerr.Code() {
-						default:
-							fmt.Fprintf( w, aerr.Error() )
-						}
-				} else {
-						// Print the error, cast err to awserr.Error to get the Code and
-						// Message from an error.
-					fmt.Fprintf( w, err.Error() )
-				}
-				return
+			w.WriteHeader(http.StatusInternalServerError)
+			if aerr, ok := err.(awserr.Error); ok {
+					switch aerr.Code() {
+					default:
+						fmt.Fprintf( w, aerr.Error() )
+					}
+			} else {
+					// Print the error, cast err to awserr.Error to get the Code and
+					// Message from an error.
+				fmt.Fprintf( w, err.Error() )
+			}
+			return
 		}
 
-		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 		json.NewEncoder( w ).Encode( result )
 	} )
 	log.Fatal(http.ListenAndServe(":8080", nil))
